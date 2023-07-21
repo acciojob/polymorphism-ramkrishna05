14f16c5df2a7a798f26b6973d4a8448d7b328b7d
ramkrishna05 <108403796+ramkrishna05@users.noreply.github.com>
2023-07-22 03:20:24 +0530

    change first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        Product p=new Product();
+        p.product(5,10);
+        p.product(5,10,15);
+        p.product(5.0,10.0);
+    }
+
+    class  Product{
+        public int product(int x,int y)
+        {
+                return x*y;
+        }
+        public int product(int x,int y,int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x,double y)
+        {
+            return x*y;
+        }
+    }
 
 }
